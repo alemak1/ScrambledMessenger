8d86c4a9291cf4a4f963bb6051f4effd1bbd7f1c
yongrenwang <czie.alexmak@gmail.com>
2017-11-19 18:39:55 +0800

    Initial Commit

---
+++ Scrambled Messenger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scrambled Messenger
+//
+//  Created by Aleksander Makedonski on 11/19/17.
+//  Copyright © 2017 Aleksander Makedonski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
